e5e8df7b0555b201c1aec4742aa6c710bbccb839
Franco Barbato <franco.barbato09@gmail.com>
2022-03-27 20:46:17 -0300

    ItemListContainer

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ItemListContainer() {
+    return(
+        <>
+            <div>
+                <p>Lista de productos</p>
+            </div>
+        </>
+    );
+}
+
+export default ItemListContainer;
